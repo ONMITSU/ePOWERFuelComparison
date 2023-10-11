6c2d7d46ef6e650413acbb6a5763acb4569ce4ab
Yossapon Jantarote <ja.yossapon_st@tni.ac.th>
2023-10-11 21:10:00 +0700

    Add caltex component

---
+++ YOSSAPONJ.FUELCOMPARISON.WEB/Pages/DisplayCaltexFuelBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using YOSSAPONJ.FUELCOMPARISON.WEB.Models;
+using YOSSAPONJ.FUELCOMPARISON.WEB.Services.Contracts;
+
+namespace YOSSAPONJ.FUELCOMPARISON.WEB.Pages
+{
+    public class DisplayCaltexFuelBase : ComponentBase
+    {
+        [Inject]
+        public ICaltexFuelPriceService CaltexFuelPriceService { get; set; }
+        public CaltexFuelPriceModel FuelPrice { get; set; }
+        public List<CaltexFuelListModel> FuelList { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            FuelPrice = await CaltexFuelPriceService.GetFuelPrice();
+            FuelList = FuelPrice.Fuels.Where(x => !x.FuelName.ToUpper().Contains("DIESEL")).ToList();
+        }
+    }
+}
