c29c5b376fc4208d8e174884977f555ae082b1af
Yossapon Jantarote <ja.yossapon_st@tni.ac.th>
2023-10-11 22:13:13 +0700

    Add fuel station selector.

---
+++ YOSSAPONJ.FUELCOMPARISON.WEB/Pages/FuelPrice/FuelPriceBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace YOSSAPONJ.FUELCOMPARISON.WEB.Pages.FuelPrice
+{
+    public class FuelPriceBase : ComponentBase
+    {
+        public Station SelectedStation { get; set; }
+
+        public enum Station
+        {
+            Bangchak,
+            Caltex
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+        }
+    }
+}
